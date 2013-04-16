15bcb8b102193a4df575901008421801b734716e
fiorenzo <fiorenzino@gmail.com>
2013-04-16 19:52:46 +0200

    1.2.0-SNAPSHOT

--- src/main/java/org/twiliofaces/request/TwilioCaller.java
+++ src/main/java/org/twiliofaces/request/TwilioCaller.java
@@ -8,6 +8,7 @@ package org.twiliofaces.request;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -20,43 +21,52 @@ import com.twilio.sdk.resource.instance.Call;
 
 @Named
 @RequestScoped
-public class TwilioCaller {
-	/**
-	 * 
-	 * @param twilioNumber
-	 * @param to
-	 * @param sid
-	 * @param token
-	 * @param twilioUrl
-	 */
-	public void simpleCall(String twilioNumber, String to, String sid,
-			String token, String twilioUrl) {
-		try {
-			TwilioRestClient client = new TwilioRestClient(sid, token);
-			Account mainAccount = client.getAccount();
-			CallFactory callFactory = mainAccount.getCallFactory();
-			Map<String, String> callParams = new HashMap<String, String>();
-			callParams.put("To", to);
-			callParams.put("From", twilioNumber);// twilioNumber
-			callParams.put("Url", twilioUrl);
-			Call call = callFactory.create(callParams);
-			// Print the call SID (a 32 digit hex like CA123..)
-			System.out.println(call.getSid());
-		} catch (TwilioRestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+public class TwilioCaller
+{
 
-	}
+   Logger logger = Logger.getLogger(getClass().getName());
 
-	/*
-	 * insert all params inside Map (like From,To, url)
-	 */
-	public Call call(String sid, String token, Map<String, String> callParams)
-			throws TwilioRestException {
-		TwilioRestClient client = new TwilioRestClient(sid, token);
-		Account mainAccount = client.getAccount();
-		CallFactory callFactory = mainAccount.getCallFactory();
-		return callFactory.create(callParams);
-	}
+   /**
+    * 
+    * @param twilioNumber
+    * @param to
+    * @param sid
+    * @param token
+    * @param twilioUrl
+    */
+   public void simpleCall(String twilioNumber, String to, String sid,
+            String token, String twilioUrl)
+   {
+      try
+      {
+         TwilioRestClient client = new TwilioRestClient(sid, token);
+         Account mainAccount = client.getAccount();
+         CallFactory callFactory = mainAccount.getCallFactory();
+         Map<String, String> callParams = new HashMap<String, String>();
+         callParams.put("To", to);
+         callParams.put("From", twilioNumber);// twilioNumber
+         callParams.put("Url", twilioUrl);
+         Call call = callFactory.create(callParams);
+         // Print the call SID (a 32 digit hex like CA123..)
+         logger.info("call SID: " + call.getSid());
+      }
+      catch (TwilioRestException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+   }
+
+   /*
+    * insert all params inside Map (like From,To, url)
+    */
+   public Call call(String sid, String token, Map<String, String> callParams)
+            throws TwilioRestException
+   {
+      TwilioRestClient client = new TwilioRestClient(sid, token);
+      Account mainAccount = client.getAccount();
+      CallFactory callFactory = mainAccount.getCallFactory();
+      return callFactory.create(callParams);
+   }
 }
