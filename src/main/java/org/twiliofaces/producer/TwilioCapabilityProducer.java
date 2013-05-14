9d5089b7ff8171dca766e3e42a53e95e587cc865
fiorenzo <fiorenzino@gmail.com>
2013-05-14 10:30:02 +0200

    start to complete jsClient: this is the token for the browser

--- src/main/java/org/twiliofaces/producer/TwilioCapabilityProducer.java
+++ src/main/java/org/twiliofaces/producer/TwilioCapabilityProducer.java
@@ -6,6 +6,9 @@
  */
 package org.twiliofaces.producer;
 
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.faces.application.Application;
@@ -17,48 +20,58 @@ import org.twiliofaces.annotations.configuration.TwilioClientToken;
 import org.twiliofaces.annotations.configuration.TwilioSid;
 import org.twiliofaces.annotations.configuration.TwilioToken;
 
+import com.twilio.sdk.client.TwilioCapability;
+import com.twilio.sdk.client.TwilioCapability.DomainException;
+
 public class TwilioCapabilityProducer {
 
 	@Inject
 	FacesContext facesContext;
 
+	@Inject
 	@TwilioSid
 	String twilioSid;
 
+	@Inject
 	@TwilioToken
 	String twilioToken;
 
+	@Inject
 	@ApplicationSid
 	String applicationSid;
 
-	// @TwilioClientToken(client = "#{params.name}")
+	// @TwilioClientToken(client = "#{loginController.username}")
+	// String flowerToken;
+	// oppure
+	// @TwilioClientToken(client = "client")
 	// String flowerToken;
 
 	@Produces
 	@TwilioClientToken
 	public String getTwilioClientToken(InjectionPoint injectionPoint) {
-		Application app = facesContext.getApplication();
-		facesContext.getViewRoot();
-		// ExpressionFactory exprFactory = app.getExpressionFactory();
-		// ELContext elContext = facesContext.getELContext();
-		//
-		// ValueExpression valExpr =
-		// exprFactory.createValueExpression(elContext,
-		// "#{params.name}", Object.class);
-		// Boolean developmentMode = (Boolean) valExpr.getValue(elContext);
-		// TwilioCapability capability = new TwilioCapability(twilioSid,
-		// twilioToken);
-		// if (applicationSid != null && !applicationSid.isEmpty())
-		// capability.allowClientOutgoing(applicationSid);
-		// String client = injectionPoint.getAnnotated()
-		// .getAnnotation(TwilioClientToken.class).client();
-		// capability.allowClientIncoming(client);
-		//
-		// try {
-		// return capability.generateToken();
-		// } catch (DomainException e) {
-		// e.printStackTrace();
-		// }
+		TwilioCapability capability = new TwilioCapability(twilioSid,
+				twilioToken);
+		if (applicationSid != null && !applicationSid.isEmpty())
+			capability.allowClientOutgoing(applicationSid);
+
+		String client = injectionPoint.getAnnotated()
+				.getAnnotation(TwilioClientToken.class).client();
+		if (client.contains("#{") && client.contains("}")) {
+			Application app = facesContext.getApplication();
+			facesContext.getViewRoot();
+			ExpressionFactory exprFactory = app.getExpressionFactory();
+			ELContext elContext = facesContext.getELContext();
+			ValueExpression valExpr = exprFactory.createValueExpression(
+					elContext, client, Object.class);
+			client = (String) valExpr.getValue(elContext);
+		}
+		capability.allowClientIncoming(client);
+
+		try {
+			return capability.generateToken();
+		} catch (DomainException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
