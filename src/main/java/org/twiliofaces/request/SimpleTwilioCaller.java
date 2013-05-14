2a153a8be226d89a6d15c9753336dee53967c99d
fiorenzo <fiorenzino@gmail.com>
2013-05-14 10:09:19 +0200

    distinct twiliocaller/twiliosmssender: contextual or not contextual

--- src/main/java/org/twiliofaces/request/SimpleTwilioCaller.java
+++ src/main/java/org/twiliofaces/request/SimpleTwilioCaller.java
@@ -6,17 +6,73 @@
  */
 package org.twiliofaces.request;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
-public class SimpleTwilioCaller extends AbstractTwilioCaller {
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.CallFactory;
+import com.twilio.sdk.resource.instance.Account;
+import com.twilio.sdk.resource.instance.Call;
+
+public class SimpleTwilioCaller {
 
 	Logger logger = Logger.getLogger(getClass().getName());
 
-	private String to;
-	private String endpoint;
 	private String from;
 	private String accountSid;
 	private String authToken;
+	
+	private String to;
+	private String endpoint;
+
+	private Map<String, String> callParams;
+
+	public SimpleTwilioCaller() {
+
+	}
+
+	public String simpleCall(String from, String to, String accountSid,
+			String authToken, String endpoint) throws TwilioRestException {
+		setFrom(from).setTo(to).setAuthToken(authToken)
+				.setAccountSid(accountSid);
+		return call();
+	}
+
+	public String call() throws TwilioRestException {
+		TwilioRestClient client = new TwilioRestClient(getAccountSid(),
+				getAuthToken());
+		Account mainAccount = client.getAccount();
+		CallFactory callFactory = mainAccount.getCallFactory();
+		getCallParams().put("To", getTo());
+		getCallParams().put("From", getFrom());// twilioNumber
+		getCallParams().put("Url", getEndpoint());
+		Call call = callFactory.create(callParams);
+		return call.getSid();
+	}
+
+	public Call call(String accountSid, String authToken,
+			Map<String, String> callParams) throws TwilioRestException {
+		setAuthToken(authToken).setAccountSid(accountSid).setCallParams(
+				callParams);
+		TwilioRestClient client = new TwilioRestClient(getAccountSid(),
+				getAuthToken());
+		Account mainAccount = client.getAccount();
+		CallFactory callFactory = mainAccount.getCallFactory();
+		return callFactory.create(getCallParams());
+	}
+
+	public Map<String, String> getCallParams() {
+		if (callParams == null)
+			this.callParams = new HashMap<String, String>();
+		return callParams;
+	}
+
+	public SimpleTwilioCaller setCallParams(Map<String, String> callParams) {
+		this.callParams = callParams;
+		return this;
+	}
 
 	public String getFrom() {
 		return from;
--- src/main/java/org/twiliofaces/request/TwilioCaller.java
+++ src/main/java/org/twiliofaces/request/TwilioCaller.java
@@ -6,8 +6,6 @@
  */
 package org.twiliofaces.request;
 
-import java.util.logging.Logger;
-
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -18,68 +16,16 @@ import org.twiliofaces.annotations.configuration.TwilioToken;
 
 @Named
 @RequestScoped
-public abstract class TwilioCaller extends AbstractTwilioCaller {
-
-	Logger logger = Logger.getLogger(getClass().getName());
-
-	private String to;
-	private String endpoint;
+public class TwilioCaller extends SimpleTwilioCaller {
 
 	@Inject
-	@TwilioNumber
-	private String from;
-	@Inject
-	@TwilioSid
-	private String accountSid;
-	@Inject
-	@TwilioToken
-	private String authToken;
-
-	public String getFrom() {
-		return from;
+	public TwilioCaller(@TwilioNumber String from,
+			@TwilioSid String accountSid, @TwilioToken String authToken) {
+		super.setFrom(from);
+		super.setAuthToken(authToken);
+		super.setAccountSid(accountSid);
 	}
 
-	public String getAccountSid() {
-		return accountSid;
-	}
-
-	public String getAuthToken() {
-		return authToken;
-	}
-
-	public String getTo() {
-		return to;
-	}
-
-	public TwilioCaller setTo(String to) {
-		this.to = to;
-		return this;
-	}
-
-	public String getEndpoint() {
-		return endpoint;
-	}
-
-	public TwilioCaller setEndpoint(String endpoint) {
-		this.endpoint = endpoint;
-		return this;
-	}
-
-	@Override
-	public AbstractTwilioCaller setFrom(String from) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public AbstractTwilioCaller setAccountSid(String accountSid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public AbstractTwilioCaller setAuthToken(String authToken) {
-		// TODO Auto-generated method stub
-		return null;
+	public TwilioCaller() {
 	}
 }
